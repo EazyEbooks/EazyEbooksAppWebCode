87ddc89485af092598a89b01958e82efdbbe53bd
EazyEbooks <eazyebook.in@gmail.com>
2025-02-06 18:24:07 +0530

    Add files via upload

--- resources/general/get_started.py
+++ resources/general/get_started.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template, session, redirect, url_for
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from pymongo import MongoClient
 
 blp = Blueprint(
     "get_started",
@@ -8,6 +9,17 @@ blp = Blueprint(
     description="Get Started page"
 )
 
+client = MongoClient("mongodb+srv://doadmin:t5iJma1p2I647Y98@db-mongodb-eazy-ebooks-63048b6d.mongo.ondigitalocean.com/eazyebooks?tls=true&authSource=admin&replicaSet=db-mongodb-eazy-ebooks")
+db = client["eazyebooks"]
+users_collection = db["users"]
+
+@blp.before_request
+def make_session_permanent():
+    session.permanent = True
+
 @blp.route("/get-started")
 def get_started():
+    if "user_id" in session:
+        return redirect(url_for("home.Home"))
+    
     return render_template("get-started.html")
